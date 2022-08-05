3fa87252b186974dc1f9045d6cdc77171d8ed083
Vladyslav <jrlimueno@gmail.com>
2022-08-05 14:29:27 +0300

    initial

---
+++ js/scritpt.js
@@ -0,0 +1,115 @@
+'use strict';
+
+// const obj = {
+//     anna: 500,
+//     alice: 800,
+//     clients: {
+//         nastya: 300,
+//         valya: 5000
+//     }
+// };
+
+// function makeCopy(mainOnj) {
+
+//     for (let key in mainOnj) {
+//         copyObj[key] = mainOnj[key];
+//     }
+
+//     return copyObj;
+// }
+
+// makeCopy(obj);
+
+// let objCopy = Object.assign({ }, obj);
+
+// const objCopy = {...obj};
+
+// const objCopy = JSON.parse(JSON.stringify(obj));
+
+// objCopy.clients.nastya = 1000;
+
+// console.log(obj);
+// console.log(objCopy);
+
+
+
+
+
+// let arr = ['alica', 'monika', 'anna'];
+
+// const newArr = arr.slice();
+// const newArr = [...arr];
+// let newArr = [];
+
+// function copyArr(arrr) {
+
+//     for (let value in arrr) {
+//         newArr[value] = arrr[value];
+//     }
+//     return newArr;
+// }
+
+// copyArr(arr);
+// newArr.splice(1, 1, 'germiona');
+
+// console.log(arr);
+// console.log(newArr);
+
+
+// const question = +prompt('How old are you?', '');
+
+// if (question > 18) {
+//     alert('Welcome');
+// } else {
+//     alert('See you later');
+// }
+
+// console.log(typeof(question));
+
+// console.log(question + 50);
+
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('How old are you?', '');
+
+
+// console.log(typeof(answers));
+
+
+
+// const zero = prompt('What is your name?', '');
+// const first = prompt('How old are you?', '');
+
+// alert(`My name is ${zero} and i am ${first} years old`);
+
+
+// const category = 'toys';
+// console.log(`hhtp://someurl.com/${category}/5`);
+
+
+
+// const abs = (1 > 2) ? 'Vasya' : 'Valya';
+// console.log(abs);
+
+
+const num = 100;
+
+switch(num) {
+    case 50:
+        console.log('more');
+        break;
+    case 180:
+        console.log('less');
+        break;
+    case 100:
+        console.log('done');
+        break;
+    default:
+        console.log('not');
+}
+
+
+
+
